1c7a98f72f34f42f57cccdd6cfe6115c63c472e5
Ismail Hasan <xplosion331@gmail.com>
2016-11-03 18:24:35 -0700

    Made a shop class. Made ingredients required to make a sandwich. Shop also tells you what ingredients you need.

--- Sandwich Typer/src/SandwichTyper.java
+++ Sandwich Typer/src/SandwichTyper.java
@@ -1,38 +1,39 @@
 import java.util.*;
 
 public class SandwichTyper {
+	
+
+	private static final String[] String = null;
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		Scanner scanner = new Scanner(System.in);
-		String sandwichchoice = scanner.next();
 		
 		//Introduction to the game.
 		System.out.println("Welcome to Sandwich Typer!");
 		System.out.println("In this game, you will be able to make the sandwich of your liking!");
 		System.out.println("What sandwich would you like to make?");
+		String sandwichchoice = scanner.next();
 		
-		//This is where the real fun begins. 
+		
+		//This is where the real fun begins.
 		switch(sandwichchoice){
 			
 		case "pbj": sandwichchoice = "pbj";
 		System.out.println("You have chosen to make a Peanut Butter and Jelly sandwich.");
 		System.out.println("You will need to visit the shop in order to obtain the ingredients to make the sandwich.");
-		
-		case "balony": sandwichchoice = "balony";
-		
-		case "blt": sandwichchoice = "blt";
-		
-		case "breakfast": sandwichchoice = "breakfast";
-		
-		case "grilledcheese": sandwichchoice = "grilledcheese";
-		
+		System.out.println("Would you like to visit the shop? (y/n)");
 		}
+			if(scanner.next().equals("y")){
+				shop.shop();
+			}
+			else{
+				System.out.println("Thanks!");
+			}
+			
+			
+		}	
 		
-		
-		
-
-	}
-
 }
